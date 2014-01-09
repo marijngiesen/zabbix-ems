1d2ea256d78063fd9164e6c57c541b7cc5e4fac7
Marijn Giesen <marijn@studio-donder.nl>
2014-01-09 11:20:05 +0100

    Work in progress

---
+++ zems/haproxy.py
@@ -0,0 +1,17 @@
+from lib.socketcollector import *
+
+class HAProxy:
+	def __init__(self):
+		pass
+
+	@staticmethod
+	def collect():
+		parameters = SocketCollectorParameters()
+		parameters.socketFile = "/var/lib/haproxy/stats.sock"
+		parameters.command = "show info\n"
+
+		collector = SocketCollector(parameters)
+		return collector.get()
+
+
+print HAProxy.collect()
