f46e400ce999cd2c7fb0bc6efabb961e7b5c7d5d
Marijn Giesen <marijn@studio-donder.nl>
2014-01-14 14:23:20 +0100

    Improvements

--- zems/haproxy.py
+++ zems/haproxy.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-from zems.lib.socketcollector import *
+from lib.socketcollector import *
 
 
 class HAProxy:
@@ -10,25 +9,7 @@ class HAProxy:
     def collect():
         parameters = SocketCollectorParameters()
         parameters.socketFile = "/var/lib/haproxy/stats.sock"
+        parameters.command = "show info\n"
 
         collector = SocketCollector(parameters)
         return collector.get()
-=======
-from lib.socketcollector import *
-
-class HAProxy:
-	def __init__(self):
-		pass
-
-	@staticmethod
-	def collect():
-		parameters = SocketCollectorParameters()
-		parameters.socketFile = "/var/lib/haproxy/stats.sock"
-		parameters.command = "show info\n"
-
-		collector = SocketCollector(parameters)
-		return collector.get()
-
-
-print HAProxy.collect()
->>>>>>> origin/master
