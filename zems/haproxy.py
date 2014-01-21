da77cb19e58bb1cb9d8143f5db5e18d6eba2d4f0
Marijn Giesen <marijn@studio-donder.nl>
2014-01-22 00:11:04 +0100

    Improvements

--- zems/haproxy.py
+++ zems/haproxy.py
@@ -9,7 +9,7 @@ class HAProxy:
     def collect():
         parameters = SocketCollectorParameters()
         parameters.socketFile = "/var/lib/haproxy/stats.sock"
-        parameters.command = "show info\n"
+        parameters.command = "show info\nshow stat\nshow errors\nshow sess\n"
 
         collector = SocketCollector(parameters)
         return collector.get()
