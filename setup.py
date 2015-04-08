64a7bc6899add97e8f52f4f900d97cb2f163d901
marijngiesen <marijn@studio-donder.nl>
2015-04-08 10:10:21 +0200

    Add Apache and HAProxy templates

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     license='GPL',
     author='Marijn Giesen',
     author_email='marijn@studio-donder.nl',
-    description='ZEMS (Zabbix Extended Monitoring Scripts) is a tool to retrieve all sorts of metrics from applications and deliver it to Zabbix in a generic way.',
+    description='ZEMS (Zabbix Extended Monitoring Scripts) is a tool to retrieve all sorts of metrics from '
+                'applications and deliver it to Zabbix in a generic way.',
     entry_points={
         'console_scripts': [
             'zems = zems.main:main',
